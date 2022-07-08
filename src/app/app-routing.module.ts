e9805fea3fd3bda481ad56395014961cee6a05a1
Ramiro Linde Rodríguez <55402909+RamiroLinde@users.noreply.github.com>
2022-07-08 02:58:26 +0200

    Creacion componente team y su estructura html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DetailsComponent } from './components/details/details.component';
 import { HomeComponent } from './components/home/home.component';
+import { TeamComponent } from './components/team/team.component';
 
 const routes: Routes = [
   { path: "", pathMatch: 'full', redirectTo:'/home'},
   { path:"home", component: HomeComponent},
-  { path:"detail/:id", component: DetailsComponent}
+  { path:"detail/:id", component: DetailsComponent},
+  { path: "team", component: TeamComponent}
 ];
 
 @NgModule({
